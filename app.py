c1f0a793da2cb073e6f11ce3ba0d111f923a7a62
sssdeanvendetta <sharanjitsembhi@gmail.com>
2026-07-20 04:10:57 -0500

    Initial commit: Raspberry Pi health server

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+# Database connection
+db = mysql.connector.connect(
+    host="127.0.0.1",
+    user="health_user",
+    password=os.getenv("DB_PASSWORD"),
+    database="health_manager"
+)
+
+# Insert data
+@app.route('/add', methods=['POST'])
+def add_data():
+    data = request.json
+    cursor = db.cursor()
+
+    query = """
+    INSERT INTO health_metrics 
+    (systolic, diastolic, blood_sugar_mg_dl, weight_lb, height_in, bmi)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+
+    values = (
+        data['systolic'],
+        data['diastolic'],
+        data['blood_sugar'],
+        data['weight'],
+        data['height'],
+        data['bmi']
+    )
+
+    cursor.execute(query, values)
+    db.commit()
+
+    return jsonify({"message": "Data inserted successfully"})
+
+# Get data
+@app.route('/data', methods=['GET'])
+def get_data():
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM health_metrics ORDER BY date_time DESC")
+    result = cursor.fetchall()
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
